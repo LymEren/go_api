1c38dc8912010612287cb7d96b9802c555b3fa16
Eyyub Eren <eyyub.erenn@gmail.com>
2022-06-15 16:00:16 +0300

    master

---
+++ configs/env.go
@@ -0,0 +1,20 @@
+package configs
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func EnvMongoURI() string {
+	// Read env file and load that env
+	err := godotenv.Overload()
+
+	if err != nil {
+		log.Fatalln("Error in .env")
+	}
+	// That returns MongoDB adress in .env
+	mongoURI := os.Getenv("MONGOURI")
+	return mongoURI
+}
---
+++ configs/setup.go
@@ -0,0 +1,40 @@
+package configs
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Client {
+	// Created new client
+	client, err := mongo.NewClient(options.Client().ApplyURI(EnvMongoURI()))
+
+	if err != nil {
+		log.Fatal("EnvMongoURI Error:", err)
+	}
+
+	//System is try to connect in 20 seconds
+	ctx, _ := context.WithTimeout(context.Background(), 20*time.Second)
+	err = client.Connect(ctx)
+
+	// Added ping for the test
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalln("Client Ping (setup.go)", err)
+	}
+	// If there are no errors program will run
+	return client
+}
+
+// We have defined global variable DB *mongo.Client data type
+var DB *mongo.Client = ConnectDB()
+
+// And we created Collections function. Its same type to Tables in sql databases
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	// Created client database what name is "golangAPI"
+	return client.Database("golangAPI").Collection(collectionName)
+}
